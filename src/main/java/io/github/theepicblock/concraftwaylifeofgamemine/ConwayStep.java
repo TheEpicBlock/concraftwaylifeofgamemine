c4988b4708cc7b365463660a68cd9a143d2d85b4
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-06-21 16:41:00 +0200

    This isn't working

--- src/main/java/io/github/theepicblock/concraftwaylifeofgamemine/ConwayStep.java
+++ src/main/java/io/github/theepicblock/concraftwaylifeofgamemine/ConwayStep.java
@@ -3,6 +3,7 @@ package io.github.theepicblock.concraftwaylifeofgamemine;
 import it.unimi.dsi.fastutil.objects.ObjectArraySet;
 import nerdhub.cardinal.components.api.component.ComponentProvider;
 import net.minecraft.block.Blocks;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
@@ -15,7 +16,9 @@ import java.util.Set;
 public class ConwayStep {
     private final World world;
     private final Block2DbyLayer aliveBlocks = new Block2DbyLayer(10);
+    private final Block2DbyLayer extraBlocks = new Block2DbyLayer(10);
     private final List<ChunkPos> indexedChunks = new ArrayList<>();
+    private final List<ChunkPos> indexedExtraChunks = new ArrayList<>();
 
     public ConwayStep(World world) {
         this.world = world;
@@ -28,6 +31,15 @@ public class ConwayStep {
         aliveBlocks.putAll(chunkAliveHolder.getAliveBlocks());
     }
 
+    public void addExtra(BlockPos pos) {
+        Chunk chunk = world.getChunk(pos);
+
+        indexedExtraChunks.add(chunk.getPos());
+
+        AliveBlockHolder chunkAliveHolder = getAliveHolderFromChunk(chunk);
+        extraBlocks.putAll(chunkAliveHolder.getAliveBlocks());
+    }
+
     public void doStuff() {
         if(!(world.getDimension() == DimensionType.getOverworldDimensionType())) return;
         System.out.println("DOING STUFF!!");
@@ -35,30 +47,40 @@ public class ConwayStep {
             System.out.println("processing layer: " + layer);
             Set<BlockPos2D> newList = updateLayer(blockList);
             List<BlockPos2D> toRemove = new ArrayList<>(blockList);
+
+            //Magic stuff to update the blocks
             toRemove.removeAll(newList);
             newList.removeAll(blockList);
 
             toRemove.forEach((pos) -> world.setBlockState(pos.to3D(layer), Blocks.AIR.getDefaultState()));
-            newList.forEach((pos) -> world.setBlockState(pos.to3D(layer), ConwayMain.CONWAY_GAME_OF_BLOCK.getDefaultState()));
+            newList.forEach((pos) -> {
+                BlockPos p = pos.to3D(layer);
+                if (world.getBlockState(p).isAir())
+                world.setBlockState(p, ConwayMain.CONWAY_GAME_OF_BLOCK.getDefaultState());
+            });
         });
     }
 
-    public static Set<BlockPos2D> updateLayer(Set<BlockPos2D> alive) {
+    private Set<BlockPos2D> updateLayer(Set<BlockPos2D> alive) {
         Set<BlockPos2D> toUpdate = new ObjectArraySet<>();
         Set<BlockPos2D> newAlive = new ObjectArraySet<>();
         alive.forEach((pos) -> BlockPos2D.addNeighbours(pos,toUpdate));
-        toUpdate.forEach((pos) -> {
+
+
+
+        //Handle conway stuff
+        for (BlockPos2D pos : toUpdate) {
             //see https://en.wikipedia.org/wiki/Conway_game#Rules (the condensed rules)
             int c = countNeighbours(alive, pos);
             if (c == 3) {
                 newAlive.add(pos);
-                return;
+                continue;
             }
             boolean isAlive = alive.contains(pos);
             if (isAlive && c == 2) {
                 newAlive.add(pos);
             }
-        });
+        }
         return newAlive;
     }
 
